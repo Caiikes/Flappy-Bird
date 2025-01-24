33e468fa5d5bd6757a5740b6e7863fe812638e63
Caiikes <caikemoreira0@gmail.com>
2025-01-24 10:06:48 -0300

    Parte 3

--- jogo.js
+++ jogo.js
@@ -2,140 +2,162 @@
 const sprites = new Image();
 sprites.src = "./sprites.png";
 
+const som_batida = new Audio();
+som_batida.src = './sons/punch.wav'
+
 const TelaInicio = {
-    desenha(){
-        Cidade.desenha();
-        Chao.desenha();
-        FlappyBird.desenha();
-        Inicio.desenha();
+    Desenha() {
+        Cidade.Desenha();
+        Chao.Desenha();
+        FlappyBird.Desenha();
+        Inicio.Desenha();
     },
-    click(){
+    Click() {
         TelaAtiva = TelaJogo;
     }
 }
 
 const TelaJogo = {
-    desenha(){
-        Cidade.desenha();
-        Chao.desenha();
-        FlappyBird.desenha();
+    Desenha() {
+        Cidade.Desenha();
+        Chao.Desenha();
+        FlappyBird.Desenha();
         FlappyBird.Atualizar();
     },
-    click(){}
+    Click() {
+        FlappyBird.Pula();
+    }
 }
 
 const canvas = document.querySelector("#game-canvas");
 const contexto = canvas.getContext("2d");
 var TelaAtiva = TelaInicio;
 
+function FazColisao(){
+    if (FlappyBird.Y + FlappyBird.Altura >= Chao.Y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 const FlappyBird = {
-    spriteX: 0,
-    spriteY: 0,
-    largura: 35,
-    altura: 25,
-    x: 10,
-    y: 50,
-        desenha(){
-            contexto.drawImage(
-                sprites,
-                FlappyBird.spriteX, FlappyBird.spriteY,
-                FlappyBird.largura, FlappyBird.altura,
-                FlappyBird.x, FlappyBird.y,
-                FlappyBird.largura, FlappyBird.altura,
-            );
-        },
-        Gravidade: 0.25,
-        Velocidade: 0,
-        Atualizar(){
-            FlappyBird.Velocidade += FlappyBird.Gravidade;
-            FlappyBird.y = FlappyBird.y + FlappyBird.Velocidade;
+    SpriteX: 0,
+    SpriteY: 0,
+    Largura: 35,
+    Altura: 25,
+    X: 10,
+    Y: 50,
+    Pulo: 4.6,
+    Pula() {
+        FlappyBird.Velocidade = -FlappyBird.Pulo;
+    },
+    Desenha() {
+        contexto.drawImage(
+            sprites,
+            FlappyBird.SpriteX, FlappyBird.SpriteY,
+            FlappyBird.Largura, FlappyBird.Altura,
+            FlappyBird.X, FlappyBird.Y,
+            FlappyBird.Largura, FlappyBird.Altura,
+        );
+    },
+    Gravidade: 0.25,
+    Velocidade: 0,
+    Atualizar() {
+        if (FazColisao()) {
+            som_batida.play();
+            TelaAtiva = TelaInicio;
+            return;
         }
+        FlappyBird.Velocidade += FlappyBird.Gravidade;
+        FlappyBird.Y = FlappyBird.Y + FlappyBird.Velocidade;
+    }
 
 };
 
 const Chao = {
-    spriteX: 0,
-    spriteY: 610,
-    largura: 224,
-    altura: 112,
-    x: 0,
-    y: 368,
-        desenha(){
-            contexto.drawImage(
-                sprites,
-                Chao.spriteX, Chao.spriteY,
-                Chao.largura, Chao.altura,
-                Chao.x, Chao.y,
-                Chao.largura, Chao.altura,
-            );
+    SpriteX: 0,
+    SpriteY: 610,
+    Largura: 224,
+    Altura: 112,
+    X: 0,
+    Y: 368,
+    Desenha() {
+        contexto.drawImage(
+            sprites,
+            Chao.SpriteX, Chao.SpriteY,
+            Chao.Largura, Chao.Altura,
+            Chao.X, Chao.Y,
+            Chao.Largura, Chao.Altura,
+        );
 
-            contexto.drawImage(
-                sprites,
-                Chao.spriteX, Chao.spriteY,
-                Chao.largura, Chao.altura,
-                Chao.x+Chao.largura, Chao.y,
-                Chao.largura, Chao.altura,
-            );
+        contexto.drawImage(
+            sprites,
+            Chao.SpriteX, Chao.SpriteY,
+            Chao.Largura, Chao.Altura,
+            Chao.X + Chao.Largura, Chao.Y,
+            Chao.Largura, Chao.Altura,
+        );
 
-        }
+    }
 };
 
 const Cidade = {
-    spriteX: 390,
-    spriteY: 0,
-    largura: 274,
-    altura: 202, 
-    x: 0,
-    y: 280,
-        desenha(){
-            contexto.drawImage(
-                sprites,
-                Cidade.spriteX, Cidade.spriteY,
-                Cidade.largura, Cidade.altura,
-                Cidade.x, Cidade.y,
-                Cidade.largura, Cidade.altura,
-            );
+    SpriteX: 390,
+    SpriteY: 0,
+    Largura: 274,
+    Altura: 202,
+    X: 0,
+    Y: 280,
+    Desenha() {
+        contexto.drawImage(
+            sprites,
+            Cidade.SpriteX, Cidade.SpriteY,
+            Cidade.Largura, Cidade.Altura,
+            Cidade.X, Cidade.Y,
+            Cidade.Largura, Cidade.Altura,
+        );
 
-            contexto.drawImage(
-                sprites,
-                Cidade.spriteX, Cidade.spriteY,
-                Cidade.largura, Cidade.altura,
-                Cidade.x+Cidade.largura, Cidade.y,
-                Cidade.largura, Cidade.altura,
-            );
+        contexto.drawImage(
+            sprites,
+            Cidade.SpriteX, Cidade.SpriteY,
+            Cidade.Largura, Cidade.Altura,
+            Cidade.X + Cidade.Largura, Cidade.Y,
+            Cidade.Largura, Cidade.Altura,
+        );
 
-        }
+    }
 };
 
 const Inicio = {
-    spriteX: 130,
-    spriteY: 0,
-    largura: 180,
-    altura: 152, 
-    x: 70,
-    y: 70,
-        desenha(){
-            contexto.drawImage(
-                sprites,
-                Inicio.spriteX, Inicio.spriteY,
-                Inicio.largura, Inicio.altura,
-                Inicio.x, Inicio.y,
-                Inicio.largura, Inicio.altura,
-            );
-        }
+    SpriteX: 130,
+    SpriteY: 0,
+    Largura: 180,
+    Altura: 152,
+    X: 70,
+    Y: 70,
+    Desenha() {
+        contexto.drawImage(
+            sprites,
+            Inicio.SpriteX, Inicio.SpriteY,
+            Inicio.Largura, Inicio.Altura,
+            Inicio.X, Inicio.Y,
+            Inicio.Largura, Inicio.Altura,
+        );
+    }
 };
 
 
-function MudarTelaAtiva(){
-    TelaAtiva.click();
+function MudarTelaAtiva() {
+    TelaAtiva.Click();
 }
 
 window.addEventListener("click", MudarTelaAtiva);
 
-function loop(){
+function loop() {
     contexto.fillStyle = '#79c5ce'
-    contexto.fillRect(0,0, canvas.width, canvas.height)
-    TelaAtiva.desenha();
+    contexto.fillRect(0, 0, canvas.width, canvas.height)
+    TelaAtiva.Desenha();
     requestAnimationFrame(loop);
 };
 
